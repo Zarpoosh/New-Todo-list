b39e900008d99ec419d6ee2739ec4a3f2e0054cf
Zarpoosh <mzar67839@gmail.com>
2024-02-04 13:55:06 +0330

    delete btn

--- js/main.js
+++ js/main.js
@@ -14,20 +14,29 @@ function addTask(e) {
 }
 
 function generateNewTask() {
-  console.log(input.value.trim());
+  // console.log(input.value.trim());
   const newTask = $.createElement("div");
   newTask.innerHTML = `
-  <div class="rounded-lg bg-gray-200 p-3 flex m-3 ">
+  <div id="task" class="rounded-lg bg-gray-200 p-3 flex m-3 ">
     <span class="mt-2">
     ${input.value.trim()}
     </span>
     <div class="ml-auto">
       <button id="edit-btn" class=" bg-green-500 p-2 rounded-lg">Edit</button>
-      <button id="delete-btn" class=" bg-red-500 p-2 rounded-lg">Delete</button>
+      <button id="delete-btn" class="  bg-red-500 p-2 rounded-lg">Delete</button>
     </div>
   </div>
   `;
+
+  const deleteBtn = newTask.querySelector("#delete-btn");
+  deleteBtn.addEventListener("click", deleteTask);
+
   input.value = "";
   taskContainer.insertAdjacentElement("beforeend", newTask);
 }
+function deleteTask(event) {
+  const taskToDelete = event.target.closest("#task");
+  taskToDelete.remove();
+}
+
 addBtn.addEventListener("click", addTask);
